b20badfa1613a536b0d73191ba6699d78fc9d0b9
Patrick <trickrenzgarcia@gmail.com>
2026-01-17 08:48:50 +0800

    import better-auth to routes with openapi

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
 import { Elysia } from "elysia";
 import { CloudflareAdapter } from "elysia/adapter/cloudflare-worker";
+import { cors } from "@elysiajs/cors";
+import { openapi } from "@elysiajs/openapi";
+import { betterAuth } from "~/ctx/better-auth";
+import { OpenAPI } from "./lib/auth";
 
 export default new Elysia({
   adapter: CloudflareAdapter,
 })
+  .use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      credentials: true,
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  )
+  .use(betterAuth)
+  .use(
+    openapi({
+      documentation: {
+        components: await OpenAPI.components,
+        paths: await OpenAPI.getPaths(),
+      },
+    })
+  )
   .get("/", () => "Hello, Elysia on Cloudflare Workers!")
   .compile();
